1b453d0efa8dfe27383a986ee2386887d3be3fd4
TheHabib <ahabib6432@gmail.com>
2024-05-05 22:18:11 +0600

    entry in file_hist

--- files_add.py
+++ files_add.py
@@ -31,6 +31,27 @@ class _add_file_window(QDialog):
         
     def add_new_pat_file(self):
         
+        conn = sqlite3.connect("MEDICO.db3")
+        cursor = conn.cursor()
+
+        cursor.execute("""SELECT file_name from patients WHERE p_id = ?""", (self.patient_id))
+        prev_file = cursor.fetchone()
+        self.prev_file = str(prev_file[0])
+        remarks = "New File Add"
+        prev_due = 0
+                
+        if(self.prev_file != ''):
+            cursor.execute("SELECT * FROM payment_history WHERE p_id = ? AND file_name = ? AND payment_date >= ? ORDER BY payment_date DESC, payment_id DESC", (self.patient_id, self.prev_file, file_add_date))
+            result = cursor.fetchone()
+            print(f"result:\n{result}")
+            prev_due = round(float(result[9]), 3)
+            print(f"prev_due = {prev_due}")
+            print("printed file name and patient id")
+            final_cost += prev_due
+            if(prev_due != 0):
+                remarks = "New File Add" + "\nPrevious File Name: " + self.prev_file + "\nPrevious Due: " + prev_due + "\nPrevious Due has Been Added to Final Cost"
+        
+        
         current_date = QDate.currentDate()
         # Convert to 'yyyy-MM-dd' format
         current_date = current_date.toString("dd-MM-yyyy")
@@ -68,7 +89,7 @@ class _add_file_window(QDialog):
             conn = sqlite3.connect("MEDICO.db3")
             cursor = conn.cursor()
 
-            cursor.execute("""UPDATE patients SET file_name = ?, file_desc = ?, estd_cost = ?, discount = ?, final_cost = ?, file_add_date = ? WHERE p_id = ?""", (file_name, file_desc, calc_estimated_cost, calc_discount, final_cost, file_add_date, self.patient_id))
+            cursor.execute("""UPDATE patients SET file_name = ?, file_desc = ?, estd_cost = ?, discount = ?, final_cost = ?, file_add_date = ?, previous_due = ? WHERE p_id = ?""", (file_name, file_desc, calc_estimated_cost, calc_discount, final_cost, file_add_date, prev_due, self.patient_id))
             conn.commit()
             msg = "File added successfully.\nFile Name: " + str(file_name) + "\nEstimated Cost: " + str(calc_estimated_cost) + "\nDiscount: " + str(calc_discount) + " taka\nFinal Cost: " + str(final_cost) + "\nFile Add Date: " + file_add_date
             self.show_error_window(msg)
@@ -84,14 +105,47 @@ class _add_file_window(QDialog):
             else:
                 last_payment_id = 0
                 print(f"last payment id: {last_payment_id}")
+            """     
+            remarks = "New File Add"
+                
+            if(self.prev_file != ''):
+                cursor.execute("SELECT * FROM payment_history WHERE p_id = ? AND file_name = ? AND payment_date >= ? ORDER BY payment_date DESC, payment_id DESC", (self.patient_id, self.prev_file, file_add_date))
+                result = cursor.fetchone()
+                print(f"result:\n{result}")
+                prev_due = round(float(result[9]), 3)
+                print(f"prev_due = {prev_due}")
+                print("printed file name and patient id")
+                final_cost += prev_due
+                remarks = "New File Add" + "\nPrevious File Name: " + self.prev_file + "\nPrevious Due: " + prev_due + "\nPrevious Due has Been Added to Final Cost"
+             """    
+            
                 
             # Increment the last_payment_id
             payment_id = last_payment_id + 1
             print(f"payment id: {payment_id}")
             
-            cursor.execute("""INSERT INTO payment_history (payment_id, p_id, payment_date, payment_amount, payment_remarks, file_name, estd_cost, discount, final_cost, due) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",(payment_id, self.patient_id, file_add_date, "0", "New File Add", file_name, calc_estimated_cost, calc_discount, final_cost, final_cost))
+            cursor.execute("""INSERT INTO payment_history (payment_id, p_id, payment_date, payment_amount, payment_remarks, file_name, estd_cost, discount, final_cost, due) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",(payment_id, self.patient_id, file_add_date, "0", remarks, file_name, calc_estimated_cost, calc_discount, final_cost, final_cost))
             conn.commit()
             
+            file_hist_remarks = ''
+            if(prev_due != 0):
+                file_hist_remarks = "Previous File Name: " + self.prev_file + "\nPrevious Due: " + prev_due
+                
+            
+            cursor.execute("SELECT MAX(CAST(hist_id AS INTEGER)) FROM files_hist")
+            result = cursor.fetchone()
+            print(f"histidresult = {result}")
+            if result[0] is not None:
+                last_hist_id = result[0]
+                print(f"last history id: {last_hist_id}")
+            else:
+                last_hist_id = 0
+                print(f"last payment id: {last_hist_id}")
+                
+            
+            
+            cursor.execute("""INSERT INTO files_hist (hist_id, patient_id, file_name, file_desc, estd_cost, discount, final_cost, file_add_date, remarks) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", (last_hist_id, self.patient_id, file_name, file_desc, calc_estimated_cost, calc_discount, final_cost, file_add_date, file_hist_remarks))
+            conn.commit()
             
             
         except Exception as e:
